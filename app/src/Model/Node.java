1e2f88f7b5376a2b73fce751ac89e60b0554b7d2
ojan208 <mfauzanazhima18@gmail.com>
2022-11-26 13:09:58 +0700

    inisiasi file-file

---
+++ app/src/Model/Node.java
@@ -0,0 +1,23 @@
+public class Node {
+    private int x, y;
+    public Node(){
+        x = 0;
+        y = 0;
+    }
+    public Node(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+}
