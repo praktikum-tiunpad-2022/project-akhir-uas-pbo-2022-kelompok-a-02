2c7451afccaa478ca2ed294469246a478ad3d06f
ojan208 <mfauzanazhima18@gmail.com>
2022-11-27 11:33:26 +0700

    inisiasi Fruit.java

--- Snake/app/src/main/java/Model/Fruit.java
+++ Snake/app/src/main/java/Model/Fruit.java
@@ -1,9 +1,21 @@
 package Model;
+/**
+ * Fruit.java
+ * 
+ * Object for the fruit, will have a random spawn point (for now)
+ * 
+ */
 
 public class Fruit {
     private Node fruit;
     public Fruit(){
         fruit = new Node();
     }
-    
+
+    public void setFruit(Node fruit) {
+        this.fruit = fruit;
+    }
+    public Node getFruit() {
+        return fruit;
+    }
 }
