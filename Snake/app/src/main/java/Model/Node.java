d8b6b1edc20ae262acd8d500300c421f63ab4502
ojan208 <mfauzanazhima18@gmail.com>
2022-11-27 11:32:27 +0700

    menambahkan drawNode

--- Snake/app/src/main/java/Model/Node.java
+++ Snake/app/src/main/java/Model/Node.java
@@ -1,24 +1,57 @@
+// package Model;
 package Model;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+/** 
+ * Node.java
+ * Foundation for all the blocks that are in the game.
+ * x and y is used for coordinates.
+ * info is used to link the block with a picture
+ */
 public class Node {
     private int x, y;
+    private String info;
+    // info buat apa yang mau ditampilin
+    
+    // gambar, pembantu buat ngegambar pada guinya
     public Node(){
         x = 0;
         y = 0;
+        info = "";
     }
-    public Node(int x, int y){
+    public Node(int x, int y, String info){
         this.x = x;
         this.y = y;
+        this.info = info;
     }
+    // Setter
     public void setX(int x) {
         this.x = x;
     }
     public void setY(int y) {
         this.y = y;
     }
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    // Getter 
     public int getX() {
         return x;
     }
     public int getY() {
         return y;
+    }
+    public String getInfo() {
+        return info;
+    }
+
+    // fungsi 
+    /**
+     * drawNode
+     * used for drawing the node in the coordinates
+     */
+    public void drawNode(GraphicsContext gambar){
+        gambar.setFill(Color.web(info));
+        gambar.fillRoundRect(x*40, y*40, 40, 40, 35, 35);
     }
 }
