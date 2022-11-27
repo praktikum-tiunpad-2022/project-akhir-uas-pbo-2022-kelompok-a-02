e9859a66df40dd986102ffb4c99e305cbab48031
ojan208 <mfauzanazhima18@gmail.com>
2022-11-27 11:31:05 +0700

    inisiasi game.java

--- Snake/app/src/main/java/Controller/Game.java
+++ Snake/app/src/main/java/Controller/Game.java
@@ -1,5 +1,18 @@
 package Controller;
 
 public class Game {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 800;
+    private static final int ROWS = 100;
+    private static final int COLUMN = 100;
+    private static final int SQUARE_SIZE = WIDTH/COLUMN;
     
+    private void run(){
+
+    }
+
+    private void drawBackground(){
+        
+    }
+
 }
